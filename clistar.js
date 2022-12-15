829c6f7a5ea9fc9698da64632d835555552e9ba9
ixc7 <isaacxpreston7@gmail.com>
2022-12-15 06:40:08 -0500

    box drawing chars

--- clistar.js
+++ clistar.js
@@ -1,12 +1,19 @@
 #!/usr/bin/env bun
 
-import pkg from 'circular-natal-horoscope-js'
-const { Origin, Horoscope } = pkg
+import { Origin, Horoscope } from 'circular-natal-horoscope-js'
 import { getLatLon } from './positionStack.js'
 
-const months = ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'september', 'October', 'November', 'December']
-
 const rep = (n, char = ' ') => char.repeat(n)
+
+const months = ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'september', 'October', 'November', 'December']
+const yChar = '┃'
+const xChar = '━'
+
+let space = {
+  planet: 0,
+  sign: 0,
+  house: 0
+}
 
 const input = process.argv.slice(2)
 
@@ -16,39 +23,32 @@ if (input.length < 4) {
 }
 
 const birthPlace = input.slice(3).join(' ')
+
 const { latitude, longitude, name } = await getLatLon(birthPlace)
 
-const DOB = {
-  month: input[0] - 1, // 0=Jan, 11=Dec
-  date: input[1],
-  year: input[2]
-}
+// const DOB = {
+const  month = input[0] - 1 // 0=Jan, 11=Dec
+const  date = input[1]
+const year = input[2]
+// }
 
-const horoscope = new Horoscope({
+const results = new Horoscope({
   origin: new Origin({
-    year: DOB.year,
-    month: DOB.month,
-    date: DOB.date,
-    hour: 12,
+    year,
+    month,
+    date,
+    hour: 12, // TODO
     minute: 0,
     latitude,
     longitude
   })
-})
-
-let space = {
-  planet: 0,
-  sign: 0,
-  house: 0
-}
-
-const results = horoscope._celestialBodies.all.map(({
+})._celestialBodies.all.map(({
   label, Sign, House = { label: '' }
 }) => {
   if (label.length > space.planet) space.planet = label.length
   if (Sign.label.length > space.sign) space.sign = Sign.label.length
   if (House.label.length > space.house) space.house = House.label.length
-
+    
   return {
     planet: label,
     sign: Sign.label,
@@ -56,10 +56,12 @@ const results = horoscope._celestialBodies.all.map(({
   }
 })
 
-const header = `${months[DOB.month]} ${DOB.date}, ${DOB.year} - ${name}`
+const header = `${months[month]} ${date}, ${year} - ${name}`
 const xLen = space.planet + space.sign + space.house + 10
 const xPad = (xLen - (header.length)) / 2
-const xRow = rep(xLen, '~')
+const xRowMid = '┣' + rep(xLen - 2, xChar) + '┫' 
+const xRowDown = '┏' + rep(xLen - 2, xChar) + '┓' 
+const xRowUp = '┗' + rep(xLen - 2, xChar) + '┛' 
 const lP = rep(Math.ceil(xPad) - 1)
 const rP = rep(Math.floor(xPad) - 1)
 const pP = rep(space.planet - 'planet'.length)
@@ -68,20 +70,20 @@ const hP = rep(space.house - 'house'.length)
 
 console.clear()
 
-console.log(`${xRow}
-|${lP}\x1b[1m${header}\x1b[0m${rP}|
-${xRow}
-| Sign${sP} | Planet${pP} | House${hP} |
-${xRow}`)
+console.log(`${xRowDown}
+${yChar}${lP}\x1b[1m${header}\x1b[0m${rP}${yChar}
+${xRowMid}
+${yChar} Sign${sP} ${yChar} Planet${pP} ${yChar} House${hP} ${yChar}
+${xRowMid}`)
 
 results.forEach(i => {
   const pad = k => rep(space[k] - i[k].length)
   console.log(
-    `| \x1b[1m${i.sign}\x1b[0m${pad('sign')} ` +
-    `| ${i.planet}${pad('planet')} ` +
-    `| ${i.house}${pad('house')} |` +
+    `${yChar} \x1b[1m${i.sign}\x1b[0m${pad('sign')} ` +
+    `${yChar} ${i.planet}${pad('planet')} ` +
+    `${yChar} ${i.house}${pad('house')} ${yChar}` +
     ` #${i.sign}${i.planet}${pad('sign')}${pad('planet')}`
   )
 })
 
-console.log(xRow)
+console.log(xRowUp)
